f200eff3e9cd2ca4f01c650d6435c99af0bbbf17
Sai Krishna <33274676+saikrishnasgit@users.noreply.github.com>
2021-05-28 15:21:46 +0530

    Initial File

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xrm.Tooling.Connector;
+using System;
+
+namespace ConnectMSDataverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msdataverseURL = "https://polearn.crm.dynamics.com;";
+            string applicationID = "1676c3f0-be0b-498a-9186-425bded41fea";
+            string appSecret = "5xQ-dM~lTjYkr-Saz64lfw99g2ytp.0WKW";
+            string connectionString = "AuthType=ClientSecret; url=" + msdataverseURL + "ClientId=" + applicationID + ";" + "ClientSecret=" + appSecret;
+            CrmServiceClient crmServiceClient = new CrmServiceClient(connectionString);
+            try
+            {
+                if (crmServiceClient != null && crmServiceClient.IsReady)
+                {
+                    Console.WriteLine("\nConnection successfull.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("\nPlease make sure the Connection String is correct.");
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception " + ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
